2678802a66215e3b54b54c60a3fc7dd6f91ace4d
shaunrods <shaunrodrigues68@gmail.com>
2025-05-15 19:40:29 +0530

    added controller and properties

---
+++ src/main/java/org/kjsim/quizapppro/WebController.java
@@ -0,0 +1,13 @@
+package org.kjsim.quizapppro;
+
+import org.hibernate.annotations.ConcreteProxy;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@ConcreteProxy
+public class WebController {
+    @RequestMapping({"/", "home"})
+    public String viewHome() {
+        return "home";
+    }
+
+}
