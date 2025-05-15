98080010bd77a571d0ed8d80bc8a6277ebb804cf
shaunrods <shaunrodrigues68@gmail.com>
2025-05-15 19:25:49 +0530

    Initial commit

---
+++ src/main/java/org/kjsim/quizapppro/QuizAppProApplication.java
@@ -0,0 +1,13 @@
+package org.kjsim.quizapppro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizAppProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizAppProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kjsim/quizapppro/QuizAppProApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kjsim.quizapppro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAppProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
